3132d00b52078967d2d3c7cb9f88a598c86b7955
Regis <boudinot.regis@yahoo.com>
2016-01-26 12:48:31 -0700

    hitting my own api

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,12 +1,12 @@
-app.controller("MainController", ['$scope', '$http', ($scope, $http, $q) => {
+app.controller("MainController", ['$scope', '$http', ($scope, $http, $httpProvider) => {
   $scope.title = 'AngularJS'
   $scope.promo = '50% off!'
   $scope.product = { name: 'Getting Started With AngularJS', price: 9,  pubdate: new Date('2012', '02', '12') }
   $scope.repo = $http({
                         method: 'GET',
-                        url: 'https://api.github.com/repos/selfup/DevOpsOne'
+                        url: 'http://idea.selfup.me/api/v1/ideas'
                       }).then(function successCallback(response) {
-                        $scope.repoName = response.data.name
+                        $scope.repoName = response.data[0].quality
                       }, function errorCallback(response) {
                         console.log("OH NO")
                       })
