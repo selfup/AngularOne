eabff05fd5b7bf258bd62dbac2e5aca125025fbf
selfup <boudinot.regis@yahoo.com>
2016-01-25 19:39:56 -0700

    successful API call using Angular AJAX equivalent

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,5 +1,15 @@
-app.controller("MainController", ['$scope', ($scope) => {
+app.controller("MainController", ['$scope', '$http', ($scope, $http) => {
   $scope.title = 'AngularJS'
   $scope.promo = '50% off!'
   $scope.product = { name: 'Getting Started With AngularJS', price: 9,  pubdate: new Date('2012', '02', '12') }
+  $scope.repo = $http({
+                        method: 'GET',
+                        url: 'https://api.github.com/repos/selfup/DevOpsOne'
+                      }).then(function successCallback(response) {
+                        console.log(response.data.name)
+                        $scope.repoName = response.data.name
+                      }, function errorCallback(response) {
+                        console.log("OH NO")
+                      })
+  $scope.repoName = $scope.repo
 }])
