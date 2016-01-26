9047a03055929d4dc3c54163d427caf071da523c
selfup <boudinot.regis@yahoo.com>
2016-01-25 19:19:03 -0700

    date filter

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,5 +1,5 @@
 app.controller("MainController", ['$scope', ($scope) => {
   $scope.title = 'AngularJS'
   $scope.promo = '50% off!'
-  $scope.product = { name: 'Getting Started With AngularJS', price: 9 }
+  $scope.product = { name: 'Getting Started With AngularJS', price: 9,  pubdate: new Date('2012', '02', '12') }
 }])
