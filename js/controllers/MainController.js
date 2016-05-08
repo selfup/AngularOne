2d6a7985242408e511ba964bf0a6f0e3ffaa0fe6
Regis <boudinot.regis@yahoo.com>
2016-05-08 15:28:32 -0600

    clean code

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,29 +1,26 @@
-app.controller("MainController", ['$scope', '$http', ($scope, $http) => {
-  $scope.color = { change: "orange"}
-  $scope.title = 'AngularJS'
-  $scope.promo = '50% off!'
-  $scope.product = {
-                     name: 'Getting Started With AngularJS',
-                     price: 9,
-                     pubdate: new Date('2012', '02', '12')
-                   }
-
-  $scope.repo = $http({
-                        method: 'GET',
-                        url: 'http://idea.selfup.me/api/v1/ideas'
-                      }).then(function successCallback(response) {
-                        $scope.repoName = response.data[0]
-                      }, function errorCallback(response) {
-                        console.log("OH NO")
-                      })
-  $scope.repoName = $scope.repo
-  $scope.colorCounter = 1
-  $scope.changeColor = function(index) {
+app.controller("MainController", ['$scope', '$http', (s, h) => {
+  s.color = { change: "orange"}
+  s.title = 'AngularJS'
+  s.promo = '50% off!'
+  s.product = { name: 'Getting Started With AngularJS',
+                price: 9,
+                pubdate: new Date('2012', '02', '12') }
+  s.repo = h({ 
+               method: 'GET',
+               url: 'http://idea.selfup.me/api/v1/ideas'
+             }).then(function successCallback(response) {
+               s.repoName = response.data[0]
+             }, function errorCallback(response) {
+               console.log(response)
+             })
+  s.repoName = s.repo
+  s.colorCounter = 1
+  s.changeColor = function(index) {
     this.colorCounter += 1
     if (this.colorCounter % 2 === 0) {
-      $scope.color.change = "teal"
+      s.color.change = "teal"
     } else {
-      $scope.color.change = "orange"
+      s.color.change = "orange"
     }
   }
 }])
