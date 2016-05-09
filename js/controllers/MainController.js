fcded6442ed6cbb27026ed58b376496000b73eba
Regis <boudinot.regis@yahoo.com>
2016-05-09 15:33:20 -0600

    cleanerrr

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,10 +1,11 @@
 app.controller("MainController", ['$scope', '$http', (s, h) => {
-  s.color = { change: "orange"}
-  s.title = 'AngularJS'
-  s.promo = '50% off!'
+  s.color   = { change: "orange"}
+  s.title   = 'AngularJS'
+  s.promo   = '50% off!'
   s.product = { name: 'Getting Started With AngularJS',
                 price: 9,
                 pubdate: new Date('2012', '02', '12') }
+                
   s.repo = h({ 
                method: 'GET',
                url: 'http://idea.selfup.me/api/v1/ideas'
@@ -13,9 +14,11 @@ app.controller("MainController", ['$scope', '$http', (s, h) => {
              }, function errorCallback(response) {
                console.log(response)
              })
-  s.repoName = s.repo
+             
+  s.repoName     = s.repo
   s.colorCounter = 1
-  s.changeColor = function(index) {
+  
+  s.changeColor  = function(index) {
     this.colorCounter += 1
     if (this.colorCounter % 2 === 0) {
       s.color.change = "teal"
